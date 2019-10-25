d5ab794e5cdb5e1e0510351a35006d3cfdf00177
Chandan Raj <56737052+codejack-CR@users.noreply.github.com>
2019-10-25 08:28:19 +0530

    Beautified code

--- time.c
+++
@@ -1,4 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-void main(){short hour=0,minute=0,second=0;while(1){system("cls");printf("%x : %x : %x ",hour,minute,second);second++;if(second==60){minute+=1;second=0;}if(minute==60){hour+=1;minute=0;}if(hour==24){hour=0;minute=0;second=0;}sleep(1);}}
