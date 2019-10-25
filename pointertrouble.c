5a54c7cd5fca1d5bdc10ce2959ecfc5c89c5a3c4
Chandan Raj <56737052+codejack-CR@users.noreply.github.com>
2019-10-25 08:19:07 +0530

    Example of common pointer usage mistake
    Pointers in C are meant to reference the memory address of a certain variable. However, in certain scenarios, the reference may get stuck in a different stack generated by some other function in your code and it may never refer back to the address that you originally sent. This simple C program aims at recreating that.

---
+++ pointertrouble.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void dupPointer(int*);
+int* dupPointerFix(int*);
+
+int main(void){
+	int *ptr;
+	//dupPointer(ptr);
+	int* ptr2 = dupPointerFix(ptr);  //Either return a pointer or skip the funtion altogether
+	//ptr = (int *)malloc(sizeof(int));
+	//*ptr = 5;
+	*ptr2 = 5;   //This statement is fine.
+	//printf("%d\n",*ptr);
+	printf("%d\n",*ptr2);
+	if(ptr==NULL)printf("\tPOINTER TROUBLES\n");
+	getchar();
+}
+
+/*void dupPointer(int* p){
+	p = (int *)malloc(sizeof(int));
+}*/
+
+int* dupPointerFix(int* p){
+	p = (int *)malloc(sizeof(int));
+	return p;
+}
+
